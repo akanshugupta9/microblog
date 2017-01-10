26b0d4a35ae7db29f6beedc853959bb8ff655d7a
Akanshu Gupta <akanshugupta9@gmail.com>
2017-01-10 21:21:29 +0530

    Part 7: Unit Testing

--- config.py
+++ config.py
@@ -8,10 +8,17 @@ OPENID_PROVIDERS = [
     { 'name' : 'Yahoo', 'url' : 'https://me.yahoo.com'},
     { 'name' : 'AOL', 'url' : 'http://openid.aol.com/<username>'},
     { 'name' : 'Flickr', 'url' : 'http://www.flickr.com/<username>'},
-    { 'name' : 'MyOpenId', 'url' : 'http://www.flickr.com/<username>'}
+    { 'name' : 'MyOpenId', 'url' : 'https://www.myopenid.com'}
 ]
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join( basedir, 'app.db')
 SQLALCHEMY_MIGRATE_REPO = os.path.join( basedir, 'db_repository')
+
+MAIL_SERVER = 'localhost'
+MAIL_PORT = 25
+MAIL_USERNAME = None
+MAIL_PASSWORD = None
+
+ADMINS = ['akg@something.com']
